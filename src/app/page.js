7be0d8056a03a095822937c68802ac0cba3784e0
klenilsonrox <klenilsoncoc@gmail.com>
2023-11-18 16:19:02 -0300

    att

--- src/app/page.js
+++ src/app/page.js
@@ -9,9 +9,13 @@ const [id,setid]=React.useState("")
 
 
   return (
-    <div className='flex flex-col max-w-[1000px]'>
-    <input type="text" value={id} onChange={({target})=>setid(target.value)} className='border'/>
-      <a href={`/${id}`} className='bg-slate-800 text-white max-w-[200px] text-center my-2 px-4 py-2'>criar</a>
+    <div className='flex flex-col items-center justify-center mt-2 max-w-[700px] mx-auto p-2 border'>
+    <div className='flex flex-col p-2 max-w-[600px] border'>
+        <h1>crie seu portfolio com apenas 1 clique</h1>
+        <input type="text" value={id} onChange={({target})=>setid(target.value)} className='border max-w-[300px]' placeholder='nome de usuário github'/>
+      <a href={`/${id}`} className='bg-slate-800 text-white max-w-[300px] text-center my-2 px-4 py-2'>criar</a>
+    </div>
+    
     </div>
   )
 }
