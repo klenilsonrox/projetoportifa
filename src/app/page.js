2ee69d020088d36788cb1074a54024e766df0e87
klenilsonrox <klenilsoncoc@gmail.com>
2023-11-21 16:07:27 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,15 @@
-"use client"
-
 import React from 'react'
 
-
-
 const page = () => {
-const [id,setid]=React.useState("")
-
-
-
   return (
-    <div className='flex w-screen h-screen flex-col relative items-center justify-center bg-[#020817]'>
-    <div className='flex flex-col p-2 max-w-[600px] animadown'>
-        <h1 className='text-white text-center text-[65px] leading-[50px] font-semibold mb-2'>Git<span className='text-[#3B82F6]'>fólio</span> </h1>
-        <p className='text-slate-400 mb-2'>crie seu portfolio com apenas 1 clique</p>
-        <input type="text" value={id} onChange={({target})=>setid(target.value)} className='border max-w-[300px] py-2 outline-none' placeholder='nome de usuário github'/>
-      <a href={`/${id}`} className=' text-white max-w-[300px] text-center my-2 px-4 py-2 bg-[#3B82F6] hover:bg-[#3b83f6ef]'>criar</a>
-    </div>
-    <footer className='absolute bottom-0 left-0 right-0 h-[50px] flex items-center justify-center'>
-      <p className='text-white '>Desenvolvido por <a href="https://www.linkedin.com/in/clenilson-brandao/" target='_blanck' className='text-blue-400'>Klenilson Rox</a></p>
-    </footer>
+    <div className='bg-black h-screen w-sreen'>
+    <h1 className='text-center text-white bg-slate-700 py-2'>Root Lyout</h1>
+      <div className='flex items-center w-screen h-[96%] justify-center'>
+      <div className='text-white flex flex-col items-center'>
+        <h1 className='text-[25px]'>Seja muito bem vindo</h1>
+        <button className='border border-white py-2 px-6 mt-2'><a href="/blog">Ir para o BLOG</a></button>
+      </div>
+      </div>
     </div>
   )
 }
